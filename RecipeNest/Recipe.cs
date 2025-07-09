2bc4cf100013bcee5ccdadb63f3c7d4aedf698a2
jakrol <kozak10555@gmail.com>
2025-07-09 17:39:00 +0200

    Learning on passing data and binding for viewmodel

---
+++ RecipeNest/Recipe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeNest
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public List<string> Ingredients { get; set; }
+        public string Instructions { get; set; }
+        public string ImageUrl { get; set; }
+        public Recipe(string name, string category, string description, string ingredients, string instructions, string imageUrl)
+        {
+            Name = name;
+            Category = category;
+            Description = description;
+            Ingredients = ingredients.Split(',').Select(i => i.Trim()).ToList();
+            Instructions = instructions;
+            ImageUrl = imageUrl;
+        }
+    }
+}
--- RecipeNest/RecipesPage.xaml.cs
+++ RecipeNest/RecipesPage.xaml.cs
@@ -1,9 +1,17 @@
+using RecipeNest.ViewModels;
 namespace RecipeNest;
 
 public partial class RecipesPage : ContentPage
 {
+	public RecipeViewModel ViewModel = new RecipeViewModel();
 	public RecipesPage()
 	{
 		InitializeComponent();
-	}
+		this.BindingContext = ViewModel;
+    }
+
+    private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+		
+    }
 }
